ddc5b2cde2d889f96a931296190f9807331c864e
jepsonr <jepsonr@gmail.com>
2015-10-13 09:05:25 -0600

    Create Exercise 14-18.cpp

---
+++ Exercise 14-18.cpp
@@ -0,0 +1,26 @@
+//Introduction to Programming with C++ (6th ed) <Diane Zak>
+//Chapter 13 figures
+//created/revised by <Rex Jepson> <date>
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //declare variables
+
+    // enter input items
+
+    //enter function
+
+    //display output
+
+    cin.get();
+    return 0;
+}//end main function
