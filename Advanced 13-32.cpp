fc75e964ec3aae9f2d4bbf84a695412a758d45b1
jepsonr <jepsonr@gmail.com>
2015-10-13 14:41:29 -0600

    added 2nd layer "do while"
    Still having dryer problem

--- Advanced 13-32.cpp
+++ Advanced 13-32.cpp
@@ -14,8 +14,10 @@ int main()
     getline(cin, word);
     while ( word != "-1" )
     {
+        cout << word << endl;
         //enter the loop to remove first letter and
         //concatenate it to pigWord
+
         while (word.substr(0, 1) != "A"
             && word.substr(0, 1) != "a"
             && word.substr(0, 1) != "E"
@@ -26,16 +28,37 @@ int main()
             && word.substr(0, 1) != "o"
             && word.substr(0, 1) != "U"
             && word.substr(0, 1) != "u"
-            && word.substr(0, 1) != "")
+            && word.substr(0, 1) != ""
+            )
         {
-            if ( word.substr(0, 1) != "Y" || word.substr(0, 1) != "y" )
+            do
             {
+                //cout << "Current substring " << word.substr(0, 1) << endl;
                 pigWord.insert(pigWord.length(), word.substr(0, 1));
                 word = word.erase(0, 1);
-            }//end if
+
+                //cout << "pigword = " << pigWord  << endl;
+                //cout << "this is the word " << word << endl;
+            }   while (word.substr(0, 1) != "A"
+                    && word.substr(0, 1) != "a"
+                    && word.substr(0, 1) != "E"
+                    && word.substr(0, 1) != "e"
+                    && word.substr(0, 1) != "I"
+                    && word.substr(0, 1) != "i"
+                    && word.substr(0, 1) != "O"
+                    && word.substr(0, 1) != "o"
+                    && word.substr(0, 1) != "U"
+                    && word.substr(0, 1) != "u"
+                    && word.substr(0, 1) != ""
+                    && word.substr(0, 1) != "Y"
+                    && word.substr(0, 1) != "y"
+                     );
         }//end while
 
-        if (pigWord.length() == 0)
+        if ( word.substr(0, 1) == "Y" || word.substr(0, 1) == "y" )
+            cout << "Translation: " << word + "-" + pigWord + "ay"
+                << endl;
+        else if (pigWord.length() == 0)
             cout << "Translation: " << word + "-way"
                 << endl;
         else if ( word.length() != 0  )
@@ -47,8 +70,22 @@ int main()
         pigWord = "";
         cout << "Enter word (-1 to exit): ";
         getline(cin, word);
+
     }//end while
 
     cin.get();
     return 0;
 }//end main function
+
+
+
+                //if ( word.substr(0, 1) != "Y" && word.substr(0, 1) != "y" )
+                //{
+                    //cout << "Current substring " << word.substr(0, 1) << endl;
+                //    pigWord.insert(pigWord.length(), word.substr(0, 1));
+                //    word = word.erase(0, 1);
+
+                    //cout << "pigword = " << pigWord  << endl;
+                    //cout << "this is the word " << word << endl;
+                //}
+                //cout <<word.substr(0, 1);
