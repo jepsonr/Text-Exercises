4025b95c6a59905778a6d54e207d64bd1d759573
jepsonr <jepsonr@gmail.com>
2015-10-14 08:57:12 -0600

    Update Exercise 14-22.cpp

--- Exercise 14-22.cpp
+++ Exercise 14-22.cpp
@@ -1,1 +1,26 @@
+//Introduction to Programming with C++ (6th ed) <Diane Zak>
+//Chapter 14 Exercise 22 Intermediate22
+//created/revised by <Rex Jepson> <10-18-2015>
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //declare variables
+
+    // enter input items
+
+    //enter function
+
+    //display output
+
+    cin.get();
+    return 0;
+}//end main function
